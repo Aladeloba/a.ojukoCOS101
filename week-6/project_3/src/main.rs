ff46cce3fc5b561531dd3d99e59bf6d3a4302bf8
a.ojukoCOS101 <aladelobaojuko@gmail.com>
2023-11-24 20:12:02 +0100

    My Seventh Commit

--- week-6/project_3/src/main.rs
+++ week-6/project_3/src/main.rs
@@ -6,11 +6,12 @@ fn main() {
   io::stdin().read_line(&mut input1).expect("Failed to read input");
   let input1:u32 = input1.trim().parse().expect("Failed to read input");
 
-  for lhs in 1..=input1 {
-    for rhs in 1..=12 {
+for lhs in 1..=input1 {
+    for rhs in 1..=12{
         let result = lhs * rhs;
         println!("{} x {} = {}", lhs, rhs, result);
     }
     println!();
-  }
+}
+
 }
